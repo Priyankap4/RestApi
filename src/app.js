d7e66663d8fc44e05087e7b6b9c0c0a7473b6cd0
Priyanka-Prajapatii <priyanka.prajapaatii@gmail.com>
2021-12-16 22:12:37 +0530

    Create Students and reading the student data

--- src/app.js
+++ src/app.js
@@ -1,35 +1,24 @@
 const express = require("express");
-const path  = require("path");
 const app = express();
-const hbs = require('hbs');
+
 require(`./db/conn`);
 const Student = require('./models/student');
 
 const port = process.env.PORT || 3000;
-const static_path = path.join(__dirname, "../public");
-const template_path = path.join(__dirname, "../template/views");
-const partials_path = path.join(__dirname, "../template/partials");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-app.set('view engine', 'hbs');
-app.set('views',template_path);
-app.use(express.static(static_path));
-
-hbs.registerPartials(partials_path);
 
 app.get('/', (req,res)=>{
     res.render('index');
 });
 
 // read the data of registered Students
-app.get('/studentDetails', (req,res)=>{
-    res.render('studentDetails');
-});
+// app.get('/studentdetails', (req,res)=>{
+//     res.render('studentdetails');
+// });
 
 // create a new student
-app.post('/studentDetails', async(req,res)=>{
+app.post('/studentdetails', async(req,res)=>{
     try{
         const studentData = new Student({
         Name : req.body.Name,
@@ -37,13 +26,12 @@ app.post('/studentDetails', async(req,res)=>{
         Class : req.body.Class,
         Marks : req.body.Marks,
         Feedback : [{
-            "text" : req.body.Feedback,
-            "StarRating" : req.body.StarRating
+            Comment : req.body.Comment,
+            StarRating : req.body.StarRating
         }]
-        });
-        const createdStudent = await studentData.save();
+        }).save();
 
-        res.status(201).render("index");
+        res.status(201).send(studentData);
 
     }catch(e){
         res.status(400).send(e);
@@ -51,8 +39,8 @@ app.post('/studentDetails', async(req,res)=>{
 });
 
 // Task 1
-// Reading student data individually clasified by Class
-app.get('/studentDetails/:Class', async(req,res)=>{
+// Reading student data clasified by Class (example of single param)
+app.get('/studentdetails/:Class', async(req,res)=>{
     try{
         const _class = req.params.Class;
         const studentData = await Student.find({Class: _class});
@@ -67,21 +55,65 @@ app.get('/studentDetails/:Class', async(req,res)=>{
     }
 });
 
-// Reading multiple student data clasified by Class
-// app.get('/studentDetails/:Class', async(req,res)=>{
-//     try{
-//         const _class = req.params.Class;
-//         const studentData = await Student.find({Class: _class});
+// Reading student data clasified by Class (example of multiple params)
+app.get('/studentdetails/:Class1/:Class2', async(req,res)=>{
+    try{
+        const _class1 = req.params.Class1;
+        const _class2 = req.params.Class2;
+        const studentData = await Student.find({
+            $or: [{Class:_class1},{Class:_class2}]
+        });
 
-//         if(!studentData){
-//             return res.status(500).send();
-//         }else{
-//             res.status(201).send(studentData);
-//         }
-//     }catch(e){
-//         res.status(404).send(e);
-//     }
-// });
+        if(!studentData){
+            return res.status(500).send();
+        }else{
+            res.status(201).send(studentData);
+        }
+    }catch(e){
+        res.status(404).send(e);
+    }
+});
+
+// Task 2
+
+app.get("/studentdetail/:id", async (req,res) =>
+   {
+       try {
+            const studentData = await Student.find({"_id": req.params.id})
+
+            if(!studentData){
+                return res.status(404).send();
+            }else{
+                res.status(201).send(studentData);
+                // console.log(studentData);
+            }
+
+        } catch (e) {
+            res.status(404).send(e)
+        }
+    }
+)
+// Updating the student data
+app.patch('/studentdetails/update/:id', async(req,res)=>{
+    try{
+        const studentUpdate = await Student.findByIdAndUpdate({"_id": req.params.id},{
+
+            $set:{
+               Name : req.body.Name,
+               Class : req.body.Class,
+               Feedback : {
+                   text: req.body.text,
+               }
+            },
+            new:true
+        })
+        // console.log(studentUpdate);
+        res.status(202).send(studentUpdate);
+
+    }catch(e){
+        res.status(404).send(e);
+    }
+})
 
 app.listen(port, ()=>{
     console.log(`the connection is setup at ${port}`);
--- src/models/student.js
+++ src/models/student.js
@@ -1,42 +1,10 @@
 const mongoose = require('mongoose');
 
 const FeedbackSchema = new mongoose.Schema({
-    text:{
-        type: String
-    },
-    StarRating : {
-       type: mongoose.Mixed,
-       1: Number,
-       2: Number,
-       3: Number,
-       4: Number,
-       5: Number,
-       default:{1:1, 2:1, 3:1, 4:1, 5:1},
-       toObject:{
-           getters: true,
-       },
-       toJSON:{
-           getters: true
-       },
-       get: function(r){
-        //    r is the entire ratings object
-        let items = Object.entries(r); //get an array of key/value pairs of the object like this [[1:1],[2:1]...]
-        let sum = 0; // sum of weighted ratings
-        let total = 0; //total number of ratings
-        for(let [key,value] of items){
-            total += value;
-            sum += value * parseInt(key); //multiply the total ratings by its weight i.e. key
-        }
-        return Math.round(sum/total)
-       },
-       
-       validator:(i)=>{
-           let b = [1,2,3,4,5] //valid star levels
-           let v = Object.keys(i).sort()
-           return b.every((x,j) => (v.length === b.length) && x=== parseInt(v[j]))
-       },
-       message: "Invalid Star Level"
-    }
+    Comment : String,
+    StarRating : Number
+},{
+    timestamps: true
 });
 
 const studentSchema = new mongoose.Schema({
